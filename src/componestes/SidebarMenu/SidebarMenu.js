08eb070635b66af69638ee64d4c824f606811425
cleberfarias <cleber_afd@hotmail.com>
2024-09-06 12:01:32 -0300

    ajuste meta

--- src/componestes/SidebarMenu/SidebarMenu.js
+++ src/componestes/SidebarMenu/SidebarMenu.js
@@ -1,42 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaHome, FaUser, FaChartBar, FaWallet } from 'react-icons/fa';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './SidebarMenu.css';
 
 const SidebarMenu = () => {
   return (
-    <div className="sidebar-menu">
+    <nav className="sidebar-menu">
       <div className="menu-content">
         <ul>
-          <li>
-            <Link to="/Home" className="menu-item">
+          <li className="menu-item">
+            <Link to="/Home" className="nav-link">
               <FaHome />
-              <span className="menu-text"></span>
+              <span className="menu-text">Home</span>
             </Link>
           </li>
-          
-          <li>
-            <Link to="/meta" className="menu-item">
+          <li className="menu-item">
+            <Link to="/meta" className="nav-link">
               <FaChartBar />
-              <span className="menu-text"></span>
+              <span className="menu-text">Meta</span>
             </Link>
           </li>
-          <li>
-            <Link to="/financeiro" className="menu-item">
+          <li className="menu-item">
+            <Link to="/financeiro" className="nav-link">
               <FaWallet />
-              <span className="menu-text"></span>
-            </Link>
-            <li>
-            <Link to="/login" className="menu-item">
-              <FaUser />
-              <span className="menu-text"></span>
+              <span className="menu-text">Financeiro</span>
             </Link>
           </li>
+          <li className="menu-item">
+            <Link to="/login" className="nav-link">
+              <FaUser />
+              <span className="menu-text">Login</span>
+            </Link>
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 
--- src/pages/Meta/meta.js
+++ src/pages/Meta/meta.js
@@ -1,127 +1,54 @@
 import React, { useState } from 'react';
 import './meta.css'; // Importa o CSS para estilização
 
-const Meta = () => {
-  // Estado para alternar entre formulários
-  const [formType, setFormType] = useState('relatorio');
-
-  // Estado para o formulário de Relatórios
-  const [empresaRelatorio, setEmpresaRelatorio] = useState('');
-  const [dataRelatorio, setDataRelatorio] = useState('');
-  const [tipoRelatorio, setTipoRelatorio] = useState('');
-  const [venda, setVenda] = useState(0);
-  const [reuniao, setReuniao] = useState(0);
-  const [pontos, setPontos] = useState(0);
-  const [relatorios, setRelatorios] = useState([]);
-
-  // Estado para o formulário de Agendamentos
+// Funções de Cálculo
+function Formulario({ onFormSubmit }) {
   const [empresa, setEmpresa] = useState('');
   const [data, setData] = useState('');
-  const [agendamentos, setAgendamentos] = useState(0);
-  const [meta, setMeta] = useState(0); // Novo estado para meta
-  const [metaAlcancada, setMetaAlcancada] = useState(false);
-  const [agendamentoConcluido, setAgendamentoConcluido] = useState(false);
-  const [agendamentosList, setAgendamentosList] = useState([]);
+  const [tipo, setTipo] = useState('');
+  const [venda, setVenda] = useState(false);
+  const [reuniao, setReuniao] = useState(false);
+  const [inputSalario, setInputSalario] = useState('');
 
-  // Função para alternar entre formulários
-  const toggleForm = () => {
-    setFormType(formType === 'relatorio' ? 'agendamentos' : 'relatorio');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (empresa && data && tipo && inputSalario) {
+      onFormSubmit({ empresa, data, tipo, venda, reuniao, inputSalario });
+      setEmpresa('');
+      setData('');
+      setTipo('');
+      setVenda(false);
+      setReuniao(false);
+      setInputSalario('');
+    } else {
+      alert('Por favor, preencha todos os campos.');
+    }
   };
 
-  // Função para adicionar relatório
-  const adicionarRelatorio = () => {
-    setRelatorios([...relatorios, {
-      empresa: empresaRelatorio,
-      data: dataRelatorio,
-      tipo: tipoRelatorio,
-      venda,
-      reuniao,
-      pontos
-    }]);
-    // Limpar campos após adicionar
-    setEmpresaRelatorio('');
-    setDataRelatorio('');
-    setTipoRelatorio('');
-    setVenda(0);
-    setReuniao(0);
-    setPontos(0);
-  };
-
-  // Função para adicionar agendamento
-  const adicionarAgendamento = () => {
-    setAgendamentosList([...agendamentosList, {
-      empresa,
-      data,
-      agendamentos,
-      meta,
-      metaAlcancada,
-      agendamentoConcluido
-    }]);
-    // Limpar campos após adicionar
+  const handleClear = () => {
     setEmpresa('');
     setData('');
-    setAgendamentos(0);
-    setMeta(0); // Limpar campo de meta
-    setMetaAlcancada(false);
-    setAgendamentoConcluido(false);
+    setTipo('');
+    setVenda(false);
+    setReuniao(false);
+    setInputSalario('');
   };
 
-  // Componente do Formulário de Relatório
-  const RelatorioForm = () => (
-    <>
-      <label className='texto-form'>Cadastrar Empresa</label>
-      <input
-        type="text"
-        placeholder="Empresa"
-        value={empresaRelatorio}
-        onChange={(e) => setEmpresaRelatorio(e.target.value)}
-        className="input-field"
-      />
-      <label className='texto-form'>Data</label>
-      <input
-        type="date"
-        value={dataRelatorio}
-        onChange={(e) => setDataRelatorio(e.target.value)}
-        className="input-field"
-      />
-      <label className='texto-form'>Tipo</label>
-      <input
-        type="text"
-        placeholder="Tipo"
-        value={tipoRelatorio}
-        onChange={(e) => setTipoRelatorio(e.target.value)}
-        className="input-field"
-      />
-      <label className='texto-form'>Venda</label>
-      <input
-        type="number"
-        placeholder="Venda"
-        value={venda}
-        onChange={(e) => setVenda(Number(e.target.value))}
-        className="input-field"
-      />
-      <label className='texto-form'>Reunião</label>
-      <input
-        type="number"
-        placeholder="Reunião"
-        value={reuniao}
-        onChange={(e) => setReuniao(Number(e.target.value))}
-        className="input-field"
-      />
-      <label className='texto-form'>Pontos</label>
-      <input
-        type="number"
-        placeholder="Pontos"
-        value={pontos}
-        onChange={(e) => setPontos(Number(e.target.value))}
-        className="input-field"
-      />
-    </>
-  );
+  const handleDelete = () => {
+    alert('Excluir registro...');
+  };
 
-  // Componente do Formulário de Agendamentos
-  const AgendamentosForm = () => (
-    <>
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <h2 className='texto-form'>Relatório de Pré-Vendas</h2>
+      <label className='texto-form'>Informe seu Salário</label>
+      <input
+        type="text"
+        placeholder="Salário"
+        value={inputSalario}
+        onChange={(e) => setInputSalario(e.target.value)}
+        className="input-field"
+      />
       <label className='texto-form'>Cadastrar Empresa</label>
       <input
         type="text"
@@ -130,118 +57,241 @@ const Meta = () => {
         onChange={(e) => setEmpresa(e.target.value)}
         className="input-field"
       />
-      <label className='texto-form'>Data</label>
+      <label className='texto-form'>Mês e Ano</label>
       <input
-        type="date"
+        type="text"
+        placeholder="MM/AAAA"
         value={data}
         onChange={(e) => setData(e.target.value)}
         className="input-field"
       />
-      <label className='texto-form'>Número de Agendamentos</label>
-      <input
-        type="number"
-        placeholder="Número de Agendamentos"
-        value={agendamentos}
-        onChange={(e) => setAgendamentos(Number(e.target.value))}
-        className="input-field"
-      />
-      <label className='texto-form'>Meta</label>
-      <input
-        type="number"
-        placeholder="Meta"
-        value={meta}
-        onChange={(e) => setMeta(Number(e.target.value))}
-        className="input-field"
-      />
-      <label className='label'>
-        Meta Alcançada:
-        <input type="checkbox" checked={metaAlcancada} onChange={(e) => setMetaAlcancada(e.target.checked)} className="checkbox" />
+      <label className='texto-form'>Origem do Lead</label>
+      <select value={tipo} onChange={(e) => setTipo(e.target.value)} className="input-field">
+        <option value="">Selecione o tipo</option>
+        <option value="GOOGLE ADS">GOOGLE ADS</option>
+        <option value="FALECONOSCO">FALECONOSCO</option>
+        <option value="TELEFONE">TELEFONE</option>
+        <option value="INDICAÇÕES">INDICAÇÕES</option>
+        <option value="EBOOK">EBOOK</option>
+        <option value="WEBINAR">WEBINAR</option>
+        <option value="EVENTO">EVENTO</option>
+        <option value="BASE">BASE</option>
+        <option value="OUTBOUND">OUTBOUND</option>
+      </select>
+      <label className="label">
+        Venda:
+        <input type="checkbox" checked={venda} onChange={(e) => setVenda(e.target.checked)} className="checkbox" />
       </label>
-      <label className='label'>
-        Agendamento Concluído:
-        <input type="checkbox" checked={agendamentoConcluido} onChange={(e) => setAgendamentoConcluido(e.target.checked)} className="checkbox" />
+      <label className="label">
+        Reunião:
+        <input type="checkbox" checked={reuniao} onChange={(e) => setReuniao(e.target.checked)} className="checkbox" />
       </label>
-    </>
+      <div className="button-container">
+        <button type="submit" className="submit-button">Cadastrar</button>
+        <button type="button" onClick={handleClear} className="submit-button">Limpar</button>
+        <button type="button" onClick={handleDelete} className="submit-button">Excluir</button>
+      </div>
+    </form>
   );
+}
 
-  // Componente da Tabela de Relatórios
-  const RelatoriosTable = () => (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Empresa</th>
-          <th>Data</th>
-          <th>Tipo</th>
-          <th>Venda</th>
-          <th>Reunião</th>
-          <th>Pontos</th>
-        </tr>
-      </thead>
-      <tbody>
-        {relatorios.map((relatorio, index) => (
-          <tr key={index}>
-            <td>{relatorio.empresa}</td>
-            <td>{relatorio.data}</td>
-            <td>{relatorio.tipo}</td>
-            <td>{relatorio.venda}</td>
-            <td>{relatorio.reuniao}</td>
-            <td>{relatorio.pontos}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+function InformacoesAdicionais({ relatorios, inputSalario }) {
+  const totalPontos = relatorios.reduce((acc, curr) => acc + curr.pontos, 0);
+  const totalComissao = calcularComissao(totalPontos);
+  const totalBonus = calcularBonus(totalPontos);
+  const totalSalarioComissaoEBonus = inputSalario + totalComissao + totalBonus;
 
-  // Componente da Tabela de Agendamentos
-  const AgendamentosTable = () => (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Empresa</th>
-          <th>Data</th>
-          <th>Agendamentos</th>
-          <th>Meta</th>
-          <th>Meta Alcançada</th>
-          <th>Agendamento Concluído</th>
-          <th>Total</th>
-        </tr>
-      </thead>
-      <tbody>
-        {agendamentosList.map((agendamento, index) => (
-          <tr key={index}>
-            <td>{agendamento.empresa}</td>
-            <td>{agendamento.data}</td>
-            <td>{agendamento.agendamentos}</td>
-            <td>{agendamento.meta}</td>
-            <td>{agendamento.metaAlcancada ? 'Sim' : 'Não'}</td>
-            <td>{agendamento.agendamentoConcluido ? 'Sim' : 'Não'}</td>
-            <td>
-              {agendamento.agendamentos * (agendamento.metaAlcancada ? 2 : 1) * 10}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+  function calcularComissao(pontos) {
+    let porcentagem = 0;
+    if (pontos >= 9.5 && pontos <= 19.5) {
+      porcentagem = 0.2;
+    } else if (pontos >= 20 && pontos <= 38) {
+      porcentagem = 0.5;
+    } else if (pontos >= 38.5 && pontos <= 69) {
+      porcentagem = 0.7;
+    } else if (pontos >= 69.5) {
+      porcentagem = 1;
+    }
+    return porcentagem * inputSalario;
+  }
+
+  function calcularBonus(pontos) {
+    let bonus = 0;
+    if (pontos >= 90) {
+      bonus = 1500;
+    } else if (pontos >= 80) {
+      bonus = 1000;
+    } else if (pontos >= 70) {
+      bonus = 500;
+    }
+    return bonus;
+  }
+
+  const enviarEmail = () => {
+    alert('Enviando por e-mail...');
+  };
+
+  const imprimirInformacoes = () => {
+    window.print();
+  };
+
+  return (
+    <div className="informacoes-container">
+      <p>Total de pontos: {totalPontos}</p>
+      <p>Salário: R$ {inputSalario}</p>
+      <p>Comissão: R$ {totalComissao}</p>
+      <p>Bônus: R$ {totalBonus}</p>
+      <p>Total de Salário, Comissão e Bônus: R$ {totalSalarioComissaoEBonus}</p>
+      <button onClick={enviarEmail} className="submit-button">Enviar por E-mail</button>
+      <button onClick={imprimirInformacoes} className="submit-button">Imprimir</button>
+    </div>
   );
+}
+
+function Relatorios({ relatorios }) {
+  const enviarEmail = () => {
+    alert('Enviando por e-mail...');
+  };
+
+  const imprimirRelatorios = () => {
+    window.print();
+  };
+
+  return (
+    <div className="relatorios-container">
+      <h2 className='text-titulo'>Relatórios</h2>
+      <div className="button-container">
+        <button onClick={enviarEmail} className="submit-button">Enviar por E-mail</button>
+        <button onClick={imprimirRelatorios} className="submit-button">Imprimir</button>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Empresa</th>
+            <th>Data</th>
+            <th>Tipo</th>
+            <th>Venda</th>
+            <th>Reunião</th>
+            <th>Pontos</th>
+          </tr>
+        </thead>
+        <tbody>
+          {relatorios.map((relatorio, index) => (
+            <tr key={index}>
+              <td>{relatorio.empresa}</td>
+              <td>{relatorio.data}</td>
+              <td>{relatorio.tipo}</td>
+              <td>{relatorio.venda ? 'Sim' : 'Não'}</td>
+              <td>{relatorio.reuniao ? 'Sim' : 'Não'}</td>
+              <td>{relatorio.pontos}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+const Meta = () => {
+  const [formType, setFormType] = useState('relatorio');
+  const [relatorios, setRelatorios] = useState([]);
+  const [agendamentosList, setAgendamentosList] = useState([]);
+  const [inputSalario, setInputSalario] = useState('');
+
+  const toggleForm = () => {
+    setFormType(formType === 'relatorio' ? 'agendamento' : 'relatorio');
+  };
+
+  const adicionarRelatorio = (novoRelatorio) => {
+    setRelatorios([...relatorios, novoRelatorio]);
+  };
+
+  const adicionarAgendamento = (novoAgendamento) => {
+    setAgendamentosList([...agendamentosList, novoAgendamento]);
+  };
+
+  const TotalCard = ({ relatorios }) => {
+    const totalPontos = relatorios.reduce((acc, curr) => acc + curr.pontos, 0);
+    const totalComissao = calcularComissao(totalPontos);
+    const totalBonus = calcularBonus(totalPontos);
+    const totalSalarioComissaoEBonus = inputSalario + totalComissao + totalBonus;
+
+    function calcularComissao(pontos) {
+      let porcentagem = 0;
+      if (pontos >= 9.5 && pontos <= 19.5) {
+        porcentagem = 0.2;
+      } else if (pontos >= 20 && pontos <= 38) {
+        porcentagem = 0.5;
+      } else if (pontos >= 38.5 && pontos <= 69) {
+        porcentagem = 0.7;
+      } else if (pontos >= 69.5) {
+        porcentagem = 1;
+      }
+      return porcentagem * inputSalario;
+    }
+
+    function calcularBonus(pontos) {
+      let bonus = 0;
+      if (pontos >= 90) {
+        bonus = 1500;
+      } else if (pontos >= 80) {
+        bonus = 1000;
+      } else if (pontos >= 70) {
+        bonus = 500;
+      }
+      return bonus;
+    }
+
+    return (
+      <div className="total-card">
+        <p>Total de pontos: {totalPontos}</p>
+        <p>Salário: R$ {inputSalario}</p>
+        <p>Comissão: R$ {totalComissao}</p>
+        <p>Bônus: R$ {totalBonus}</p>
+        <p>Total de Salário, Comissão e Bônus: R$ {totalSalarioComissaoEBonus}</p>
+      </div>
+    );
+  };
 
   return (
     <div className="meta-container">
+      <h1>Meta e Relatórios</h1>
       <button onClick={toggleForm} className="toggle-button">
-        Alternar Formulário
+        {formType === 'relatorio' ? 'Mostrar Agendamentos' : 'Mostrar Relatórios'}
       </button>
-
-      {/* Renderiza o formulário atual baseado no estado */}
       {formType === 'relatorio' ? (
         <>
-          <RelatorioForm />
-          <button onClick={adicionarRelatorio} className="submit-button">Adicionar Relatório</button>
-          <RelatoriosTable />
+          <Formulario onFormSubmit={adicionarRelatorio} />
+          <Relatorios relatorios={relatorios} />
+          <TotalCard relatorios={relatorios} inputSalario={inputSalario} />
         </>
       ) : (
         <>
-          <AgendamentosForm />
-          <button onClick={adicionarAgendamento} className="submit-button">Adicionar Agendamento</button>
-          <AgendamentosTable />
+          <div className="form-container">
+            <h2>Formulário de Agendamento</h2>
+            <form>
+              <label>Nome da Empresa:</label>
+              <input
+                type="text"
+                placeholder="Nome da Empresa"
+                className="input-field"
+              />
+              <label>Data do Agendamento:</label>
+              <input
+                type="text"
+                placeholder="Data"
+                className="input-field"
+              />
+              <label>Tipo de Agendamento:</label>
+              <select className="input-field">
+                <option>Selecione</option>
+                <option>Tipo 1</option>
+                <option>Tipo 2</option>
+              </select>
+              <button type="submit" className="submit-button">Cadastrar</button>
+            </form>
+          </div>
+          {/* Adicionar lógica para exibir agendamentos */}
         </>
       )}
     </div>
